b2f37d7d69ea875a52841d954f37a2a16a881af9
Jun Aruga <jaruga@redhat.com>
2025-06-02 17:23:27 +0200

    Add the case of connecting with RSA certificate in the Ruby script.

--- connect.rb
+++ connect.rb
@@ -1,13 +1,27 @@
 require 'openssl'
 require 'socket'
 
+def print_usage
+  puts <<EOF
+Usage #{$PROGRAM_NAME} [mldsa|rsa]
+EOF
+end
+
+if ARGV.length < 1
+  print_usage
+  abort
+end
+crypto=ARGV[0]
+# puts "crypto: #{crypto}"
+
 tcp_sock = nil
 ssl_sock = nil
 begin
   tcp_sock = TCPSocket.new("127.0.0.1", 4433)
   ctx = OpenSSL::SSL::SSLContext.new
-  mldsa_cert = OpenSSL::X509::Certificate.new File.read("localhost-mldsa.crt")
-  ctx.cert = mldsa_cert
+  cert = OpenSSL::X509::Certificate.new File.read("localhost-#{crypto}.crt")
+  ctx.cert = cert
+  ctx.key = nil
   ssl_sock = OpenSSL::SSL::SSLSocket.new(tcp_sock, ctx)
   ssl_sock.sync_close = true
   ssl_sock.connect
