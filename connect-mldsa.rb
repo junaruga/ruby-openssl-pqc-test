2b37e47ef74777c633cfb5fe077bc904ca25bcbb
Jun Aruga <jaruga@redhat.com>
2025-06-02 16:58:32 +0200

    Connect with ML-DSA certificate.

--- connect-mldsa.rb
+++ connect-mldsa.rb
@@ -6,6 +6,8 @@ ssl_sock = nil
 begin
   tcp_sock = TCPSocket.new("127.0.0.1", 4433)
   ctx = OpenSSL::SSL::SSLContext.new
+  mldsa_cert = OpenSSL::X509::Certificate.new File.read("localhost-mldsa.crt")
+  ctx.cert = mldsa_cert
   ssl_sock = OpenSSL::SSL::SSLSocket.new(tcp_sock, ctx)
   ssl_sock.sync_close = true
   ssl_sock.connect
