b517e8868a0f2e209ae236f4551938e5c08e80b6
Jun Aruga <jaruga@redhat.com>
2025-06-10 18:13:13 +0200

    Refactor by Rubocop.

--- start_dual_cert_server.rb
+++ start_dual_cert_server.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require 'openssl'
 require 'socket'
 
 ctx = OpenSSL::SSL::SSLContext.new
-mldsa_key = OpenSSL::PKey.read(File.read("localhost-mldsa.key"))
-mldsa_cert = OpenSSL::X509::Certificate.new(File.read("localhost-mldsa.crt"))
-rsa_key = OpenSSL::PKey.read(File.read("localhost-rsa.key"))
-rsa_cert = OpenSSL::X509::Certificate.new(File.read("localhost-rsa.crt"))
+mldsa_key = OpenSSL::PKey.read(File.read('localhost-mldsa.key'))
+mldsa_cert = OpenSSL::X509::Certificate.new(File.read('localhost-mldsa.crt'))
+rsa_key = OpenSSL::PKey.read(File.read('localhost-rsa.key'))
+rsa_cert = OpenSSL::X509::Certificate.new(File.read('localhost-rsa.crt'))
 # ctx.key = mldsa_key
 # ctx.cert = mldsa_cert
 ctx.add_certificate(mldsa_cert, mldsa_key)
 ctx.add_certificate(rsa_cert, rsa_key)
 
-tcp_server = TCPServer.new("127.0.0.1", 4433)
+tcp_server = TCPServer.new('127.0.0.1', 4433)
 ssl_server = OpenSSL::SSL::SSLServer.new(tcp_server, ctx)
 begin
   loop do
@@ -22,9 +24,9 @@ begin
       # pp ssl_connection, ssl_server
       puts "Client sent data:\n#{data.dump}" if data
     ensure
-      ssl_connection.close if ssl_connection
+      ssl_connection&.close
     end
   end
 ensure
-  tcp_server.close if tcp_server
+  tcp_server&.close
 end
